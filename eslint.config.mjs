b87dd0f8eecb15f223d612360445ee419e157593
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-04-05 15:59:00 +0900

    chore(eslint): add react, panda, security, and compiler eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
+import panda from '@pandacss/eslint-plugin';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import command from 'eslint-plugin-command/config';
+import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect';
+import security from 'eslint-plugin-security';
 import storybook from 'eslint-plugin-storybook';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -16,7 +21,32 @@ const eslintConfig = defineConfig([
     'next-env.d.ts',
     'styled-system',
   ]),
+  {
+    plugins: { '@pandacss': panda },
+    rules: {
+      ...panda.configs.recommended.rules,
+      '@pandacss/no-hardcoded-color': 'off',
+    },
+  },
   command(),
+  {
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          ignoreCase: true,
+          reservedFirst: true,
+        },
+      ],
+    },
+  },
+  reactCompiler.configs.recommended,
+  reactYouMightNotNeedAnEffect.configs.recommended,
+  security.configs.recommended,
   ...storybook.configs['flat/recommended'],
 ]);
 
