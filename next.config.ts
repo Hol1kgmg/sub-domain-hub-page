6db6e5b95e74b922c8836626874dc47855a8053c
Holikgmg <abdevs.horisho@icloud.com>
2026-04-26 09:26:34 +0900

    fix;デプロイ設定の改修

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from 'next';
 // @keep-sorted
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: true,
-  cacheLife: {
-    infinite: {
-      stale: Number.MAX_VALUE,
-      revalidate: Number.MAX_VALUE,
-      expire: Number.MAX_VALUE,
-    },
-  },
   // @keep-sorted
   logging: {
     browserToTerminal: true,
